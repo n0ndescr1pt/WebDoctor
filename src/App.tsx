e73d4e56fa235519f0b047b9b4b6447bc40faf70
nondescript <nondescript.que@gmail.com>
2023-12-22 00:19:53 +0600

    header and other

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import Header from './components/Header/Header.tsx'
+import Service from './components/ServiceCard/ServiceCard.tsx'
+import InfoPad from './components/InfoPad/InfoPad.tsx'
+import Slider from './components/Slider/Slider.tsx'
+import ArticleCard from './components/ArticleCard/ArticleCard.tsx'
+import Footer from './components/Footer/Footer.tsx'
+import './App.css'
+
+
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <Service />
+    <InfoPad />
+    <Slider />
+    <ArticleCard />
+    <Footer />
+    </>
+  )
+}
+
+export default App
